3e5c2856483a179c030139cd0ac46e31b72c04aa
Philipp Schmitt <philipp@schmitt.co>
2018-09-22 18:17:35 +0200

    Fix README format and bump version

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="sherlockbikepy",
-    version="0.1",
+    version="0.1.1",
     license="GPL3",
     description="Python library to interact with the sherlock.bike API",
     long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
     author="Philipp Schmitt",
     author_email="philipp@schmitt.co",
     url="https://github.com/pschmitt/sherlockbikepy",
